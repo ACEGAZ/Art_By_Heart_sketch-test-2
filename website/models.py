208504f472ff08fce4ddbc78d324804416d12171
ACEGAZ <garylowles@gmail.com>
2022-09-13 09:57:21 +0000

    add, add art, regular commissions, reference sheet commission, custom commissions, and comment models to models.py

--- website/models.py
+++ website/models.py
@@ -1,3 +1,80 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.urls import reverse
 # Create your models here.
+
+STATUS = ((0, "Draft"), 1, "Published")
+
+
+class add_art(models.Model):
+    """add artwork to website """
+    title = models.CharField(max_length=200, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.title
+
+
+class regular_commission(models.Model):
+    """contact form for users to request regular commisions"""
+    character_reference = models.CharField(max_length=100)
+    character_owner = models.CharField(max_length=100)
+    commission_type = models.CharField(max_length=100)
+    type_option = models.CharField(max_length=100,)
+    character_personality = models.CharField(max_length=100)
+    pose = models.CharField(max_length=100)
+    other_info = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return self.email
+
+
+class reference_sheet_commission(models.Model):
+    """contact form for users to request reference sheet commisions"""
+    character_reference = models.CharField(max_length=100)
+    character_owner = models.CharField(max_length=100)
+    design_changes = models.CharField(max_length=100)
+    add_ons = models.CharField(max_length=100)
+    other_info = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return self.email
+
+
+class custom_commissions(models.Model):
+    """contact form for users to request custom commisions"""
+    theme = models.CharField(max_length=100)
+    colours = models.CharField(max_length=100)
+    traits = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    accessories = models.CharField(max_length=100)
+    other_info = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+
+    def __str__(self):
+        return self.email
+
+
+class comment(models.Model):
+    """comment model for autharised users to comment on artwork on gallery page"""
+    post = models.ForeignKey(add_art, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0),
+    ordering = ["created_on"]
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
+
+    def get_absolute_url(self):
+        return reverse('add_comment_success')
+
+class whatever(models.Model):
+    email = models.EmailField(max_length=100)
--- website/urls.py
+++ website/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('gallery', views.display_artwork, name='gallery'),
+    path('commissions', views.commission_view, name='commissions'),
     path('accounts/', include('allauth.urls')),
-    # path('', views.index, name='index'),
-    # path('gallery', views.display_artwork, name='gallery'),
-    # path('commissions', views.commission_view, name='commissions'),
+    
+    
+    
     # path('admin_upload_art', views.upload_art_view, name='admin_upload_art'),
     # path('add_comment_success', views.add_comment_success, name='add_comment_success'),
 ]
