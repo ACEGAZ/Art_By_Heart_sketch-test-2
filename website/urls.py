607e91222d1f2422a12fd501ce64ea51216d6723
ACEGAZ <garylowles@gmail.com>
2022-09-13 09:39:21 +0000

    add and style base and index.html

--- website/urls.py
+++ website/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path, include
 from . import views
-from .views import AddCommentView, UpdateCommentView
+# from .views import AddCommentView, UpdateCommentView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('gallery', views.display_artwork, name='gallery'),
-    path('commissions', views.commission_view, name='commissions'),
-    path('admin_upload_art', views.upload_art_view, name='admin_upload_art'),
-    path('add_comment_success', views.add_comment_success, name='add_comment_success'),
-    
-
-
-
     path('accounts/', include('allauth.urls')),
+    # path('', views.index, name='index'),
+    # path('gallery', views.display_artwork, name='gallery'),
+    # path('commissions', views.commission_view, name='commissions'),
+    # path('admin_upload_art', views.upload_art_view, name='admin_upload_art'),
+    # path('add_comment_success', views.add_comment_success, name='add_comment_success'),
 ]
