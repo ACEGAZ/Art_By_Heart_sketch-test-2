c269eb6d99a5c611df659e95e58a8d54dbafe29a
ACEGAZ <garylowles@gmail.com>
2022-09-13 12:31:40 +0000

    deployment commit

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import add_art
+from .models import comment
+from .models import regular_commission
 
-# Register your models here.
+admin.site.register(regular_commission)
+admin.site.register(add_art)
+admin.site.register(comment)
--- website/urls.py
+++ website/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path, include
 from . import views
-# from .views import AddCommentView, UpdateCommentView
+from .views import AddCommentView, UpdateCommentView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('gallery', views.display_artwork, name='gallery'),
     path('commissions', views.commission_view, name='commissions'),
+    path('admin_upload_art', views.upload_art_view, name='admin_upload_art'),
+    path('add_comment', AddCommentView.as_view(), name='add_comment'),
     path('accounts/', include('allauth.urls')),
-    
-    
-    
-    # path('admin_upload_art', views.upload_art_view, name='admin_upload_art'),
-    # path('add_comment_success', views.add_comment_success, name='add_comment_success'),
 ]
